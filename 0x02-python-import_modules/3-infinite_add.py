a3b591a309874d70fdb1a8ccbc24b1fb6e06e662
AFAAF19 <afaf.mourky@gmail.com>
2023-08-11 16:25:47 -0700

    task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    "addition of one or more numbers"
+    import sys
+
+    def args_counts():
+        args = sys.argv[1:]
+        argc = len(args)
+        res = 0
+
+        for i in range(argc):
+            res += int(args[i])
+        print(res)
+args_counts()
+
