254bcc2f2b27f37898bbefc63e1575680874fb9c
AFAAF19 <afaf.mourky@gmail.com>
2023-08-11 16:51:22 -0700

    modify

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    "addition of one or more numbers"
+    """addition of one or more numbers"""
     import sys
 
     def args_counts():
         args = sys.argv[1:]
         argc = len(args)
-        resultat = 0
+        res = 0
 
         for i in range(argc):
-            resultat += int(args[i])
-        print(resultat)
+            res += int(args[i])
+        print(res)
 args_counts()
 
