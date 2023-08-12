e529b49e5a4af7bb5be114d9a9a808943afc46bc
AFAAF19 <afaf.mourky@gmail.com>
2023-08-11 17:14:44 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     import hidden_4
     lis_names = dir(hidden_4)
     for new_lis in lis_names:
-        if new_lis[:2] != "__":
-            print('{}'.format(new'lis))
+        if new_lis[:2] != '__':
+            print('{}'.format(new_lis))
