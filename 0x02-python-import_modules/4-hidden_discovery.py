02911d6aa9bdfad6545b4ad15c371c951c3a3785
AFAAF19 <afaf.mourky@gmail.com>
2023-08-11 17:08:09 -0700

    task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import hidden_4
+    lis_names = dir(hidden_4)
+    for new_lis in lis_names:
+        if new_lis[:2] != "__":
+            print('{}'.format(new'lis))
