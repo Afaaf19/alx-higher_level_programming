c1ca45a7585119b6f141a9deb4f3508629677679
AFAAF19 <afaf.mourky@gmail.com>
2023-08-14 12:39:59 -0700

    fixeed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 """function that removes all characters c and C from a string"""
 
 def no_c(my_string):
-    new_string = ""
+    new_string = ''
     for ch in my_string:
         if ch != 'c' and ch != 'C':
             new_string += ch
-            return new_string
+    return new_string
