dcb6e75d4218415bfde8e0bce1d4b8fcd3e94920
AFAAF19 <afaf.mourky@gmail.com>
2023-08-16 17:00:03 -0700

    checks fixed

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-"""function that computes the square value of all integers of a matrix"""
 
 def square_matrix_simple(matrix=[]):
     new_matrix = [[]] * len(matrix)
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-""" function that prints a dictionary by ordered keys"""
 
 def print_sorted_dictionary(a_dictionary):
     for k in sorted(a_dictionary.keys()):
