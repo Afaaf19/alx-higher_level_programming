c552a45f18c795a403a739172e47f93c8425313c
AFAAF19 <afaf.mourky@gmail.com>
2023-08-29 12:23:46 -0700

    added 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """The class square uses a private attribute size"""
+    def __init___(self, size):
+        """ Initializes a new square
+        Args:
+        size: size of the new square
+        """
+        self.__size = size
