d4dbf3b3447e0dd95750a56c770689e413319731
AFAAF19 <afaf.mourky@gmail.com>
2023-11-16 10:50:15 -0800

    task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+"""
+model that defines the Rectangle Class
+"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    Rectangle Class: subclass of Base
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Constructor
+        """
+        super().__init__(id)
+
+        self._check_integer_parameter(width, 'width')
+        self._check_integer_parameter(height, 'height')
+        self._check_integer_parameter(x, 'x')
+        self._check_integer_parameter(y, 'y')
+
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = w
+
+    @property
+    def width(self):
+        """
+        width of the rectangle: getter
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, val):
+        """
+        width of the rectangle: setter
+        """
+        self._check_integer_parameter(val, 'width')
+        self.__width = val
+
+    @property
+    def height(self):
+        """
+        height of the rectangle: getter
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, val):
+        """
+        height of the rectangle: setter
+        """
+        self._check_integer_parameter(val, 'height')
+        self.__height = val
+
+    @property
+    def x(self):
+        """x of the rectangle: getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, val):
+        """
+        x of the rectangle: setter
+        """
+        self._check_integer_parameter(val, 'x')
+        self.__x = val
+
+    @property
+    def y(self):
+        """y of the rectangle: getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, val):
+        """
+        y of the rectangle: setter
+        """
+        self._check_integer_parameter(val, 'y')
+        self.__y = val
