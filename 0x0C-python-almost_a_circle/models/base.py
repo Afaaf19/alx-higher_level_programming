1c32775bb9620ebfacbee7057bec4a18b528c0b6
AFAAF19 <afaf.mourky@gmail.com>
2023-11-17 12:59:46 -0800

    added 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
 model that contains a base class
 """
 from os import path
+import json
+from json import dumps, loads
+
 
 class Base:
     """
@@ -20,3 +23,12 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dict):
+        """
+        returns the JSON string representation of list_dictionaries
+        """
+        if list_dict is None or len(list_dict) == 0:
+            return '[]'
+        return dumps(list_dict)
