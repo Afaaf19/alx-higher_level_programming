e7060fe615ec08fc7659a49b56982b5211aa28b5
AFAAF19 <afaf.mourky@gmail.com>
2023-11-17 06:29:30 -0800

    edit display

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,7 +84,7 @@ class Rectangle(Base):
     def display(self):
         """prints in stdout the Rectangle instance with the character #"""
         if self.__y > 0:
-            print('\n' * self__y, end='')
+            print('\n' * self.__y, end='')
 
         for i in range(self.__height):
             if self.__x > 0:
